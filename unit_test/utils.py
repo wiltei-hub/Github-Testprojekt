c2bc0a7aba90ba0b40b18c42e397ec8363481201
wiltei-hub <wiltei2010@gmail.com>
2025-05-16 09:48:58 +0200

    Update utils.py
    docstring bearbeitet

--- unit_test/utils.py
+++ unit_test/utils.py
@@ -2,7 +2,8 @@
 # Prüfung EMail: gültig, wenn @ und . enthalten sind
 def ist_gueltige_email(email):
     """
-    
+    Diese Funktion prüft auf gültige EMail-Adresse 
+    Eine EMail-Adresse wird als gültig betrachtet, wenn sie das ET-Zeichen @ und einen Punkt . enthält
     :param email:
     :return:
     """
